a3967c8bde765c112bad947c0f1520c21629ac20
Argir Argirov <dj.giro@gmail.com>
2018-03-22 14:17:10 -0400

    node bitfinex api call, react call to node endpoint established

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,22 @@ import './App.css';
 
 class App extends Component {
   state = {
-    response: ''
+    last_price: ''
   };
-
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
   callApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
-
     if (response.status !== 200) throw Error(body.message);
 
     return body;
   };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ last_price: parseFloat(res.data) }))
+      .catch(err => console.log(err));
+  }
+
 
   render() {
     return (
@@ -31,7 +30,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">{this.state.response}</p>
+        <p className="App-intro">{this.state.last_price}</p>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const axios = require('axios')
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+const results = axios.get('https://api.bitfinex.com/v1/pubticker/btcusd')
+  .then(function (response) {
+    app.get('/api/hello', (req, res) => {
+      res.send({ data: response.data.last_price });
+    })
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
