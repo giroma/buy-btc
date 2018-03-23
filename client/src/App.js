2b0caf37891e2780c9bdd79bed5a87ad62e2337d
Argir Argirov <dj.giro@gmail.com>
2018-03-23 14:21:57 -0400

    input fields, quote, account balance handling complete

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,21 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      usd: 156.12,
-      btc: 0.00000000,
+      usdBalance: 156.12,
+      btcBalance: 0.00000000,
       lastPrice: undefined,
-      input: '',
+      userInput: '',
       quote: ''
     }
   }
 
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ lastPrice: parseFloat(res.data) }))
+    this.callBitfinexApi()
+      .then(res => this.setState({ lastPrice: Number(res.data) }))
       .catch(err => console.log(err));
   }
 
-  callApi = async () => {
+  callBitfinexApi = async () => {
     const response = await fetch('/api/hello');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
@@ -31,29 +31,41 @@ class App extends Component {
   };
 
   handleInputChange = (e) => {
-    this.setState({input: parseFloat(e.target.value)})
+    this.setState({userInput: Number(e.target.value)})
     this.calculateQuote(e.target.value)
   }
 
   calculateQuote = (amount) => {
-    console.log('amount', amount);
-    let lastPrice = this.state.lastPrice
-    console.log('lastPrice', lastPrice);
-    let quote = amount / lastPrice;
-    console.log('quote', quote);
-    this.setState({quote: quote.toFixed(8)})
-    if (amount === '') {this.setState({quote: ''})} //in nothing in input, return state to ''
+    const lastPrice = this.state.lastPrice
+    const quote = amount / lastPrice;
+    if (amount > 0) {
+      this.setState({quote: Number(quote.toFixed(8))})
+    } else {
+      this.setState({quote: '', userInput: ''})
+    }
   }
 
-  handleTrade = () => {
-
+  handleTrade = (e) => {
+    e.preventDefault()
+    const lastPrice = this.state.lastPrice
+    const accountUSD = this.state.usdBalance
+    const accountBTC = this.state.btcBalance
+    const userInput = this.state.userInput
+    const quote = userInput/lastPrice
+    const newDollarBalance = accountUSD - userInput
+    const newBitcoinBalance = accountBTC + quote
+    this.setState({ usdBalance: Number(newDollarBalance.toFixed(2)),
+                    btcBalance: Number(newBitcoinBalance.toFixed(8)),
+                    quote: '',
+                    userInput: ''})
   }
 
   render() {
     return (
-      <Form usd={this.state.usd}
-            btc={this.state.btc}
+      <Form usdBalance={this.state.usdBalance}
+            btcBalance={this.state.btcBalance}
             quote={this.state.quote}
+            userInput={this.state.userInput}
             handleInputChange={this.handleInputChange}
             handleTrade={this.handleTrade}/>
     );
--- client/src/Form.js
+++ client/src/Form.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-  saveAmount = () => {
-
-  }
   render() {
     return (
-      <div className="form">
+      <form className="form" onSubmit={this.props.handleTrade}>
         <div className='account'>
           <div>Account Balance</div>
           <div className="holdings">
-            <div>USD<span>{this.props.usd.toFixed(2)}</span></div>
-            <div>BTC <span>{this.props.btc.toFixed(8)}</span></div>
+            <div>USD<span>{this.props.usdBalance}</span></div>
+            <div>BTC <span>{this.props.btcBalance}</span></div>
           </div>
         </div>
         <span>Trade</span>
         <input type="text" disabled="true" placeholder="USD" />
-        <input className='input-field' type="number" max={this.props.usd} placeholder="Enter your amount" onChange={this.props.handleInputChange}/>
+        <input className='input-field' type="number" value = {this.props.userInput} minLength='1' step='any' min='1.0' max={this.props.usdBalance} placeholder="Enter your amount" onChange={this.props.handleInputChange}/>
         <span>For</span>
         <input type="text" disabled="true" placeholder="BTC"  />
         <input type="text" disabled="true" placeholder="Display Quote" value={this.props.quote}/>
-        <button className="button" onClick={this.props.handleTrade}>Trade</button>
-      </div>
+        <button className="button">Trade</button>
+      </form>
     )
   }
 }
